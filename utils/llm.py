c43603e0f1b55951929bad538f3cacc14274bfb7
Pallavi Kumari <pallaviyadav0108@gmail.com>
2026-07-19 15:11:28 +0530

    Fix Streamlit deployment

--- utils/llm.py
+++ utils/llm.py
@@ -1,12 +1,18 @@
 from dotenv import load_dotenv
 import os
+import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 def get_llm():
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+    if not api_key:
+        api_key = st.secrets["GOOGLE_API_KEY"]
+
     return ChatGoogleGenerativeAI(
-        model="gemini-3-flash-preview",
-        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        model="gemini-2.5-flash",
+        google_api_key=api_key,
         temperature=0,
     )
