64e522c4c8571e00171226edc24da51c9eca1dd7
Pallavi Kumari <pallaviyadav0108@gmail.com>
2026-07-19 18:04:11 +0530

    Update Gemini model

--- utils/llm.py
+++ utils/llm.py
@@ -7,12 +7,11 @@ load_dotenv()
 
 def get_llm():
     api_key = os.getenv("GOOGLE_API_KEY")
-
     if not api_key:
         api_key = st.secrets["GOOGLE_API_KEY"]
 
     return ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-2.0-flash",
         google_api_key=api_key,
         temperature=0,
     )
